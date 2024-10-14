1f123e8a0be666dad8250f7f28c40324b0b0aec2
NicoZapata28 <135772592+NicoZapata28@users.noreply.github.com>
2024-10-14 17:01:20 -0300

    Update footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,23 @@
+<style>
+  footer {
+    background-color: #f8f9fa;
+    padding: 1rem;
+    text-align: center;
+    border-top: 1px solid #dee2e6;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    font-size: 0.9rem;
+    color: black;
+  }
+  p{
+    margin:0;
+  }
+</style>
+
+<footer>
+  <p><strong>&copy; 2024 Boggio Valentino, Larrauri Martina, Leali Bruno, Zapata Nicolás. Todos los derechos reservados.</strong></p>
+  <p><strong>Todos los derechos reservados.</strong></p>
+  <p>PoC - Desarrollo de Software</p>
+  <p>UTN Rosario</p>
+</footer>
