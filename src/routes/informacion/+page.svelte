3d8d0209c1273a1603c8942364ccc671a85e6765
NicoZapata28 <135772592+NicoZapata28@users.noreply.github.com>
2024-10-15 20:26:13 -0300

    Create CardGrid and update Card and CardInfo

--- src/routes/informacion/+page.svelte
+++ src/routes/informacion/+page.svelte
@@ -1,6 +1,17 @@
 <script>
   import Header from '../../components/Header.svelte'
+  import CardGrid from '../../components/CardGrid.svelte'
+
+
+  let sections = [{section: "Autogestión Alumnos", icon: "../assets/user-icon.svg"},
+                  {section: "Autogestión Docentes", icon: "../assets/user-icon.svg"},
+                  {section: "CVG", icon: "../assets/user-icon.svg"}
+                ];
 </script>
 
 <Header />
-<h1>Informacion</h1>
+<h1>Información</h1>
+
+<section>
+  <CardGrid itemsInfo={sections}/>
+</section>
