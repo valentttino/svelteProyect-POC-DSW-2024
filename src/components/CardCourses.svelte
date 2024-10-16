1686c25632aa511e3d5c0486b4f1468185327707
valentttino <vgboggio@gmail.com>
2024-10-15 22:43:00 -0300

    create cardCourses

---
+++ src/components/CardCourses.svelte
@@ -0,0 +1,37 @@
+<script>
+  export let title
+</script>
+
+<style>
+  .container{
+    height: 150px;
+    width: 180px;
+    border: 2px;
+    border-color: #201E43;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .neutroColor{
+    width: 100%;
+    height: 70%;
+    background-color: #201E43;
+  }
+
+  .text{
+    width: 100%;
+    height: 30%;
+    text-align: center;
+    align-items: center;
+  }
+</style>
+
+<div class="container">
+  <div class="neutroColor">
+  </div>
+  <div class="text">
+    {title}
+  </div>
+</div>
--- src/routes/carreras/+page.svelte
+++ src/routes/carreras/+page.svelte
@@ -1,9 +1,11 @@
 <script>
   import Header from "../../components/Header.svelte"
+  import CardCourses from "../../components/CardCourses.svelte"
   import Footer from "../../components/Footer.svelte"
 </script>
 
 <Header />
 <h1>Carrreras</h1>
 
+<CardCourses title={"Ingeniería en Sistemas de Información"}/> //BORRAR
 <Footer />
