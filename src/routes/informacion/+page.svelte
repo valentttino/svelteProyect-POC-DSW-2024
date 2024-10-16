fb028c69f187b8883fcc2b5fbde33595992b39b2
valentttino <vgboggio@gmail.com>
2024-10-15 21:47:36 -0300

    create contactForm

--- src/routes/informacion/+page.svelte
+++ src/routes/informacion/+page.svelte
@@ -1,19 +1,25 @@
 <script>
   import Header from '../../components/Header.svelte'
   import CardGrid from '../../components/CardGrid.svelte'
+  import ContactForm from '../../components/ContactForm.svelte'
   import Footer from '../../components/Footer.svelte'
 
   let sections = [{section: "Autogestión Alumnos", icon: "../assets/user-icon.svg"},
                   {section: "Autogestión Docentes", icon: "../assets/user-icon.svg"},
                   {section: "CVG", icon: "../assets/user-icon.svg"}
-                ];
+                ]
 </script>
+
+<style>
+
+</style>
 
 <Header />
 <h1>Información</h1>
 
-<section>
+<section class="back">
   <CardGrid itemsInfo={sections}/>
+  <ContactForm />
 </section>
 
 <Footer />
