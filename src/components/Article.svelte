56e5d111b5de7c1d76a5d50cc6cb2aaf7f4e57ba
Bruno <brunoleali2002@gmail.com>
2024-10-17 15:05:44 -0300

    Update Article

--- src/components/Article.svelte
+++ src/components/Article.svelte
@@ -1,46 +1,51 @@
 <script>
-    export let imageSrc = '';  
-    export let title = '';    
-    export let text = '';      
-  </script>
-  
-  <style>
-    a{
-      text-decoration: none;
-      color: black;
-    }
+  export let imageSrc = '';  
+  export let title = '';    
+  export let text = '';      
+</script>
 
-    .article-container {
-      display: flex;
-      align-items: flex-start;
-      padding: 16px;
-      border-radius: 8px;
-      margin-bottom: 16px;
-    }
-  
-    .article-image {
-      width: 150px;
-      height: 150px;
-      object-fit: cover;
-      margin-right: 16px;
-    }
-  
-    .article-content {
-      flex: 1;
-    }
-  
-    .article-title {
-      font-size: 1.5em;
-      margin: 0;
-      margin-bottom: 8px;
-    }
-  
-    .article-text {
-      font-size: 1em;
-      color: #666;
-    }
-  </style>
- 
+<style>
+  a {
+    text-decoration: none;
+    color: black;
+  }
+
+  .article-container {
+    display: flex;
+    align-items: flex-start;
+    padding: 16px;
+    border-radius: 8px;
+    margin-bottom: 16px;
+    background-color: #EEEEEE;
+  }
+
+  .article-image {
+    width: 150px;
+    height: 150px;
+    object-fit: cover;
+    margin-right: 16px;
+  }
+
+  .article-content {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center; 
+  }
+
+  .article-title {
+    font-size: 1.5em;
+    margin: 0 auto;
+    margin-bottom: 8px;
+    text-align: center;
+  }
+
+  .article-text {
+    font-size: 1em;
+    color: #666;
+  }
+</style>
+
 <a href="/desarrollo">
   <div class="article-container">
     <img src={imageSrc} alt={title} class="article-image" />
--- src/routes/institucional/+page.svelte
+++ src/routes/institucional/+page.svelte
@@ -4,12 +4,20 @@
   import Carrousel from "../../components/Carrousel.svelte"
   import Article from "../../components/Article.svelte"
   import Footer from "../../components/Footer.svelte"
+  import news from "../../data/news.json"
 
 </script>
 
 <Header />
 <IntroSection text={"Institucional"} />
 <Carrousel />
-<Article imageSrc={"https://i.info7.mx/img/i7/4020000/4022879_mono-fumando.jpg"} title={"Mono de pana"} text={"Se lo ve bien de pana al pedazo de macaco este."}/>
-<Article imageSrc={"https://pbs.twimg.com/media/FTe9q3pXoAEBy3B.jpg"} title={"Mono disfrutando su cigarrito"} text={"Se lo ve bien gostoso a este buen mamífero disfrutando su enrolado de tabaco."}/>
+<div class="news-container">
+  {#each news as article (article.id)}
+    <Article 
+      imageSrc={article.urlImage} 
+      title={article.title} 
+      text={article.description}
+    />
+  {/each}
+</div>
 <Footer />
